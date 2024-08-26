a196dfda3b3a7fc34507c0bc630f231a7562abd6
oskah <oskah@uef.fi>
2024-08-26 10:21:40 +0300

    .

---
+++ part4/blogilista/utils/list_helper.js
@@ -0,0 +1,21 @@
+const dummy = (blogs) => {
+    return 1;
+  };
+  
+  const totalLikes = (blogs) => {
+    return blogs.reduce((sum, blog) => sum + blog.likes, 0);
+  };
+  
+  const favoriteBlog = (blogs) => {
+    if (blogs.length === 0) return null;
+  
+    return blogs.reduce((prev, current) => 
+      (current.likes > prev.likes) ? current : prev
+    );
+  };
+  
+  module.exports = {
+    dummy,
+    totalLikes,
+    favoriteBlog,
+  };
