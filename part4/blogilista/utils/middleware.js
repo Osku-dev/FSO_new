cbf50da0eec0cc4323d635f01ecba6e2bd9e077c
oskah <oskah@uef.fi>
2024-09-02 11:09:52 +0300

    Expanded errorHandling for future use cases

--- part4/blogilista/utils/middleware.js
+++ part4/blogilista/utils/middleware.js
@@ -16,9 +16,18 @@ const errorHandler = (error, request, response, next) => {
   logger.error(error.message)
 
   if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' })
+    return response.status(400).send({ error: 'malformatted id' });
   } else if (error.name === 'ValidationError') {
-    return response.status(400).json({ error: error.message })
+    return response.status(400).json({ error: error.message });
+  } else if (error.code === 11000) { // Duplicate key error
+    const fieldName = Object.keys(error.keyValue)[0]; // Get the field name that caused the error
+    return response.status(400).json({
+      error: `${fieldName} must be unique, '${error.keyValue[fieldName]}' already exists`
+    });
+  } else if (error.name === 'JsonWebTokenError') {
+    return response.status(401).json({ error: 'invalid token' });
+  } else if (error.name === 'TokenExpiredError') {
+    return response.status(401).json({ error: 'token expired' });
   }
 
   next(error)
