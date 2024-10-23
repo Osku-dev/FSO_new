1dfd3b027ec07600ed039feae803f1988278c873
oskah <oskah@uef.fi>
2024-10-23 10:41:58 +0300

    notificationReducer base

---
+++ part6/redux-anecdotes/src/reducers/notificationReducer.js
@@ -0,0 +1,10 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const notificationSlice = createSlice({
+    name: 'notification',
+    initialState: 'test notification',
+    reducers: {
+    },
+  })
+
+  export default notificationSlice.reducer
