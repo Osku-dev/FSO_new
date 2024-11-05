3f0730517f352aef1771edfda0756194659cd0be
oskah <oskah@uef.fi>
2024-11-05 11:53:39 +0200

    7.8 useResource custom hook

--- part7/ultimate-hooks/src/App.jsx
+++ part7/ultimate-hooks/src/App.jsx
@@ -18,11 +18,29 @@ const useField = (type) => {
 const useResource = (baseUrl) => {
   const [resources, setResources] = useState([])
 
-  // ...
+  
+  useEffect(() => {
+    const fetchResources = async () => {
+      try {
+        const response = await axios.get(baseUrl);
+        setResources(response.data);
+      } catch (error) {
+        console.error('Error fetching resources:', error);
+      }
+    };
+  
+    fetchResources();
+  }, [baseUrl]);
 
-  const create = (resource) => {
-    // ...
-  }
+  const create = async (resource) => {
+    try {
+      const response = await axios.post(baseUrl, resource);
+      return response.data;
+    } catch (error) {
+      console.error("Error creating resource:", error);
+      throw error;  
+    }
+  };
 
   const service = {
     create
