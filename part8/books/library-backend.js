23d1f476bdaeef367decdfaef1b3aa400bc6f13b
oskah <oskah@uef.fi>
2024-11-18 11:50:10 +0200

    8.6 addBook mutation

--- part8/books/library-backend.js
+++ part8/books/library-backend.js
@@ -1,5 +1,6 @@
 const { ApolloServer } = require("@apollo/server");
 const { startStandaloneServer } = require("@apollo/server/standalone");
+const { v4: uuid } = require('uuid');
 
 let authors = [
   {
@@ -101,6 +102,15 @@ type Author {
     allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
   }
+
+  type Mutation {
+  addBook(
+    title: String!
+    author: String!
+    published: Int!
+    genres: [String!]!
+  ): Book!
+}
 `;
 
 const resolvers = {
@@ -127,7 +137,36 @@ const resolvers = {
     bookCount: (root) => {
       return books.filter((book) => book.author === root.name).length;
     },
+    
   },
+  Mutation: {
+    addBook: (root, args) => {
+      const { title, author, published, genres } = args;
+
+      let existingAuthor = authors.find(a => a.name === author);
+
+      if (!existingAuthor) {
+        const newAuthor = {
+          name: author,
+          id: uuid(),
+          born: null
+        };
+        authors = authors.concat(newAuthor);
+        existingAuthor = newAuthor;
+      }
+
+      const newBook = {
+        title,
+        published,
+        author: existingAuthor.name,
+        id: uuid(),
+        genres,
+      };
+      books = books.concat(newBook);
+
+      return newBook;
+    },
+  }
 };
 
 const server = new ApolloServer({
