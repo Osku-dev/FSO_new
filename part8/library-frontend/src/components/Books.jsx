ba8515f05c2c18477ef82753b1830cce2431a880
oskah <oskah@uef.fi>
2024-11-20 10:43:54 +0200

    8.9 Books component

--- part8/library-frontend/src/components/Books.jsx
+++ part8/library-frontend/src/components/Books.jsx
@@ -1,9 +1,21 @@
+import { gql, useQuery } from '@apollo/client'
+
+const ALL_BOOKS = gql`
+  query {
+    allBooks {
+      title
+      published
+      author
+    }
+  }
+`
+
 const Books = (props) => {
+  const result = useQuery(ALL_BOOKS)
   if (!props.show) {
     return null
   }
 
-  const books = []
 
   return (
     <div>
@@ -16,7 +28,7 @@ const Books = (props) => {
             <th>author</th>
             <th>published</th>
           </tr>
-          {books.map((a) => (
+          {result.data.allBooks.map((a) => (
             <tr key={a.title}>
               <td>{a.title}</td>
               <td>{a.author}</td>
