67abd5d0bbbd3abeab9a4c3342cf37705e5151b5
oskah <oskah@uef.fi>
2024-12-11 21:27:36 +0200

    validated the original data and made the schema match it

--- part9/patientor_backend/src/utils.ts
+++ part9/patientor_backend/src/utils.ts
@@ -4,7 +4,7 @@ import { z } from "zod";
 export const newPatientEntrySchema = z.object({
   name: z.string(),
   dateOfBirth: z.string().regex(/^\d{4}-\d{2}-\d{2}$/),
-  ssn: z.string().regex(/^\d{3}-\d{2}-\d{4}$/),
+  ssn: z.string(),
   gender: z.nativeEnum(Gender),
   occupation: z.string(),
 });
